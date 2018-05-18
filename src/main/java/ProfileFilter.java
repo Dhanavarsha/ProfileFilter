373f820ff43bce8b473b1b3c2c32100edbc97676
Dhanavarsha <dhan.varsha03@gmail.com>
2018-05-18 19:38:02 +0530

    Enables selection of profile based on multiple keywords search

--- src/main/java/ProfileFilter.java
+++ src/main/java/ProfileFilter.java
@@ -1,6 +1,8 @@
 import rules.ContainsKeyword;
+import rules.Rule;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class ProfileFilter {
     private File file;
@@ -13,5 +15,14 @@ public class ProfileFilter {
 
     public boolean isSelected(ContainsKeyword rule) {
         return rule.interpret(reader.getDocumentText(file));
+    }
+
+    public boolean isSelected(ArrayList<Rule> rules) {
+        for (Rule rule : rules) {
+            if (!rule.interpret(reader.getDocumentText(file))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/ProfileFilterTests.java
+++ src/test/java/ProfileFilterTests.java
@@ -1,8 +1,10 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import rules.ContainsKeyword;
+import rules.Rule;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class ProfileFilterTests {
     @Test
@@ -18,5 +20,16 @@ public class ProfileFilterTests {
         ProfileFilter filter = new ProfileFilter(file);
         ContainsKeyword rule = new ContainsKeyword("Selenium");
         Assert.assertTrue(filter.isSelected(rule));
+    }
+
+    @Test
+    public void testForMultipleKeywords() {
+        File file = new File("src/main/resources/DD_Resume.doc");
+        ProfileFilter filter = new ProfileFilter(file);
+        ArrayList<Rule> rules = new ArrayList<Rule>();
+        rules.add(new ContainsKeyword("Selenium"));
+        rules.add(new ContainsKeyword("Appium"));
+        rules.add(new ContainsKeyword("Java"));
+        Assert.assertTrue(filter.isSelected(rules));
     }
 }
