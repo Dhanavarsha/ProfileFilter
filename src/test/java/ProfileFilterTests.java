856208d0abdd210ba9ca00a82228d4c89f7b7e7c
Dhanavarsha <dhan.varsha03@gmail.com>
2018-05-20 20:38:57 +0530

    Deletes all tests as they are migrated to respective unit tests

--- src/test/java/ProfileFilterTests.java
+++ src/test/java/ProfileFilterTests.java
@@ -6,48 +6,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class ProfileFilterTests {
-    @Test
-    public void testDocumentReader() {
-        File file = new File("src/main/resources/Test.doc");
-        Assert.assertEquals(DocumentReader.getDocumentText(file).trim(), "Hello World!");
-    }
 
-    @Test
-    public void testProfileForSingleKeyword() {
-        File file = new File("src/main/resources/DD_Resume.doc");
-        ContainsKeyword rule = new ContainsKeyword("Java");
-        Assert.assertTrue(rule.interpret(DocumentReader.getDocumentText(file)));
-    }
-
-    @Test
-    public void testProfileForMultipleKeywords() {
-        File file = new File("src/main/resources/DD_Resume.doc");
-        ArrayList<Rule> listOfRules = new ArrayList<Rule>();
-        listOfRules.add(new ContainsKeyword("Selenium"));
-        listOfRules.add(new ContainsKeyword("Appium"));
-        listOfRules.add(new ContainsKeyword("Java"));
-        Rule rule = new AllRules(listOfRules);
-        Assert.assertTrue(rule.interpret(DocumentReader.getDocumentText(file)));
-    }
-
-    @Test
-    public void testProfileForAnyOfInputKeyword() {
-        File file = new File("src/main/resources/DD_Resume.doc");
-        ArrayList<Rule> listOfRules = new ArrayList<Rule>();
-        listOfRules.add(new ContainsKeyword("Watir"));
-        listOfRules.add(new ContainsKeyword("Selenium"));
-        listOfRules.add(new ContainsKeyword("Sahi"));
-        AnyRule rule = new AnyRule(listOfRules);
-        Assert.assertTrue(rule.interpret(DocumentReader.getDocumentText(file)));
-    }
-
-    @Test
-    public void testProfileForInputKeywordAndItsCount() {
-        File file = new File("src/main/resources/DD_Resume.doc");
-        ArrayList<Rule> listOfRules = new ArrayList<Rule>();
-        listOfRules.add(new MinimumWordCount("Selenium", 3));
-        listOfRules.add(new MinimumWordCount("Appium", 3));
-        Rule rule = new AllRules(listOfRules);
-        Assert.assertTrue(rule.interpret(DocumentReader.getDocumentText(file)));
-    }
+    
 }
