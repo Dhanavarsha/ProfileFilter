7daba29c3a1e4bc71da3c6cde3352eb7982ca504
Dhanavarsha <dhan.varsha03@gmail.com>
2018-05-21 12:16:39 +0530

    Adds an integration test with a combination of different rules

--- src/test/java/ProfileFilterTests.java
+++ src/test/java/ProfileFilterTests.java
@@ -6,6 +6,52 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class ProfileFilterTests {
+    @Test
+    public void testRulesIntegration() {
+        File file = new File("src/main/resources/DD_Resume.doc");
+        String documentText = DocumentReader.getDocumentText(file);
 
-    
+        Rule rule1 = getRuleForMandatorySkills();
+        Rule rule2 = getRuleForCITools();
+
+        ArrayList<Rule> selectionRules = new ArrayList<Rule>();
+        selectionRules.add(rule1);
+        selectionRules.add(rule2);
+
+        Rule selectionRule1 = new AllRules(selectionRules);
+
+        Assert.assertTrue(selectionRule1.interpret(documentText));
+
+        Rule rule3 = getRuleForCertification();
+
+        selectionRules.add(rule3);
+
+        Rule selectionRule2 = new AllRules(selectionRules);
+
+        Assert.assertFalse(selectionRule2.interpret(documentText));
+    }
+
+    private Rule getRuleForCertification() {
+        ArrayList<Rule> certifications = new ArrayList<Rule>();
+        certifications.add(new ContainsKeyword("Certified Disciplined Agilist"));
+        certifications.add(new ContainsKeyword("PMI Agile Certified Practitioner"));
+        return new AnyRule(certifications);
+    }
+
+    private Rule getRuleForCITools() {
+        ArrayList<Rule> skillsWithOptions = new ArrayList<Rule>();
+        skillsWithOptions.add(new MinimumWordCount("Travis", 1));
+        skillsWithOptions.add(new ContainsKeyword("Jenkins"));
+        return new AnyRule(skillsWithOptions);
+    }
+
+    private Rule getRuleForMandatorySkills() {
+        ArrayList<Rule> mandatorySkills = new ArrayList<Rule>();
+        mandatorySkills.add(new MinimumWordCount("Selenium", 3));
+        mandatorySkills.add(new MinimumWordCount("Appium", 3));
+        mandatorySkills.add(new MinimumWordCount("Java", 2));
+        mandatorySkills.add(new ContainsKeyword("REST"));
+        mandatorySkills.add(new ContainsKeyword("API Testing"));
+        return new AllRules(mandatorySkills);
+    }
 }
