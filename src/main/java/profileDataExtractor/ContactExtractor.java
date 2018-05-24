73373bd800b629a2ac0645d4f65be700e310ce24
Dhanavarsha <dhan.varsha03@gmail.com>
2018-05-24 12:13:17 +0530

    Extracts out variable for country code

--- src/main/java/profileDataExtractor/ContactExtractor.java
+++ src/main/java/profileDataExtractor/ContactExtractor.java
@@ -2,9 +2,11 @@ package profileDataExtractor;
 
 class ContactExtractor extends PatternDataExtractor {
 
+    private static final String COUNTRY_CODE = "+91";
+
     @Override
     String getPattern() {
-        return "(\\+91?(\\s?|\\-?)\\d{10})|(\\d{10})|(\\(?\\+91?\\)?(\\-|\\s)?\\d{3}\\s?\\d{3}\\s?\\d{4})";
+        return "(\\" + COUNTRY_CODE + "?(\\s?|\\-?)\\d{10})|(\\d{10})|(\\(?\\" + COUNTRY_CODE + "?\\)?(\\-|\\s)?\\d{3}\\s?\\d{3}\\s?\\d{4})";
     }
 
     @Override
