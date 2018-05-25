da4749ca119b25a6a131e8fd40edf257e9cbe6f7
Dhanavarsha <dhan.varsha03@gmail.com>
2018-05-25 12:21:54 +0530

    Implements OutputFileGenerator to create output file

---
+++ src/main/java/fileWriter/OutputFileGenerator.java
@@ -0,0 +1,38 @@
+package fileWriter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OutputFileGenerator {
+    public static void writeToFile(String nameOnFile) {
+
+        File directory = new File(getOutputFolderPath());
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        File file = new File(directory + "/" + getOutputFileName(nameOnFile));
+        try {
+            FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            bw.write(nameOnFile);
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getOutputFileName(String nameOnFile) {
+        return "DONE_" + nameOnFile + "_" + getTimeStamp() + ".txt";
+    }
+
+    private static String getTimeStamp() {
+        return new SimpleDateFormat("yyyy_MM_dd_HH:mm:ss").format(new Date());
+    }
+
+    private static String getOutputFolderPath() {
+        return System.getProperty("user.dir") + "/target/selectedProfiles/";
+    }
+}
