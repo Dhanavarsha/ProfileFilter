1c09876ee870bd99ff6daa3fee417480f77ca178
Dhanavarsha <dhan.varsha03@gmail.com>
2018-06-01 15:41:25 +0530

    Creates UI for Profile Filter

---
+++ src/main/java/ui/Controller.java
@@ -0,0 +1,76 @@
+package ui;
+
+import app.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import rules.AllRules;
+import rules.Rule;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class Controller implements SkillSet {
+
+    private final DirectoryChooser chooser = new DirectoryChooser();
+    private Optional<File> file = Optional.empty();
+    private ArrayList<SkillSetView> skillSetViews = new ArrayList<>();
+
+    @FXML
+    private Label directorySelectionAlertLabel;
+
+    @FXML
+    VBox mainVBox;
+
+    @FXML
+    Label profilesDirectory;
+
+    @FXML
+    Button execute;
+
+    public void initialize() {
+        addSkillSet();
+    }
+
+    public void browseProfileDirectory(ActionEvent actionEvent) {
+        chooser.setTitle("Choose Profiles Folder location");
+        File chosenFile = chooser.showDialog(((Node) actionEvent.getTarget()).getScene().getWindow());
+        file = Optional.ofNullable(chosenFile);
+        profilesDirectory.setText(" Profiles Folders Location : "
+                + file.map((Function<File, Object>) File::getAbsolutePath).orElse(" No Directory Selected."));
+    }
+
+    public void executeRule() {
+        ArrayList<Rule> selectionRules = new ArrayList<>();
+        for (SkillSetView skillSetView : skillSetViews) {
+            selectionRules.add(skillSetView.createRule());
+        }
+
+        if (!file.isPresent()) {
+            directorySelectionAlertLabel.setText("Please enter directory");
+        } else {
+            new Application().parseProfiles(file.get(), new AllRules(selectionRules));
+        }
+    }
+
+    @Override
+    public void addSkillSet() {
+        SkillSetView skillSetView = new SkillSetView(this);
+        mainVBox.getChildren().add(skillSetView);
+        skillSetViews.add(skillSetView);
+    }
+
+    @Override
+    public void removeSkillSet(SkillSetView skillSetView) {
+        if (skillSetViews.size() != 1) {
+            mainVBox.getChildren().remove(skillSetView);
+            skillSetViews.remove(skillSetView);
+        }
+    }
+}
---
+++ src/main/java/ui/SkillView.java
@@ -0,0 +1,71 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import rules.MinimumWordCount;
+import rules.Rule;
+
+class SkillView extends HBox {
+    private final TextField enterSkillTextField;
+    private final TextField enterWordCountTextField;
+
+    SkillView(Skill skill) {
+        super();
+        this.setSpacing(20);
+
+        this.getChildren().add(new Label("Skill : "));
+
+        enterSkillTextField = getTextField("Enter Skill ex: Selenium");
+        this.getChildren().add(enterSkillTextField);
+
+        enterWordCountTextField = getTextField("Enter Word Count");
+        this.getChildren().add(enterWordCountTextField);
+
+        createAddAnotherSkillButton(this, skill);
+        createRemoveSkillButton(this, skill);
+    }
+
+    private void createAddAnotherSkillButton(SkillView skillView, Skill skill) {
+        Button addAnotherSkillButton = getButton("Add Another Skill");
+        addAnotherSkillButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                skill.addSkill();
+            }
+        });
+        skillView.getChildren().add(addAnotherSkillButton);
+    }
+
+    private void createRemoveSkillButton(SkillView skillView, Skill skill) {
+        Button removeSkillButton = getButton("Remove Skill");
+        removeSkillButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                skill.removeSkill(SkillView.this);
+            }
+        });
+        skillView.getChildren().add(removeSkillButton);
+    }
+
+    private TextField getTextField(String promptText) {
+        TextField field = new TextField();
+        field.setPromptText(promptText);
+        return field;
+    }
+
+    private Button getButton(String textOnButton) {
+        Button button = new Button();
+        button.setText(textOnButton);
+        return button;
+    }
+
+    Rule createRule() {
+        final String word = enterSkillTextField.getText();
+        final int count = Integer.parseInt(enterWordCountTextField.getText());
+        return new MinimumWordCount(word, count);
+    }
+}
